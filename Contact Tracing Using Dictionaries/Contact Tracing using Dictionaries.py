9c752acc20f33ae123036cb8fd492a74bb57017d
markred0430 <redilamarkanthony30@gmail.com>
2022-11-13 03:10:34 +0800

    Option 1

--- Contact Tracing Using Dictionaries/Contact Tracing using Dictionaries.py
+++ Contact Tracing Using Dictionaries/Contact Tracing using Dictionaries.py
@@ -1,11 +1,34 @@
 #Display a menu option
-print("Menu: " )
-print("1 -> Add an item.")
-print("2 -> Search.")
-print("3 -> Exit (y/n)")
+#print("Menu: " )
+#print("1 -> Add an item.")
+#print("2 -> Search.")
+#print("3 -> Exit (y/n)")
 #Allow the user to select item in the menu.
 selection = int(input("Select what you want to do: \n"))
 #Perform the selected option
 #Option1
+if selection == 1:
+   Full_Name = str(input("Full Name: "))
+   Age = str(input("Age: "))
+   Address = str(input("Address: "))
+   Phone_num = str(input("Phone Number: "))
+   print("Saved!")
+
+Contact_Tracing = {
+    "Full name": {
+        Full_Name
+    },
+    "Age": {
+        Age
+    },
+    "Address": {
+        Address
+    },
+    "Phone Number": {
+        Phone_num
+    }
+}
+for key, value in Contact_Tracing.items():
+    print(key, ":", value)
 #Option2
 #Option3
