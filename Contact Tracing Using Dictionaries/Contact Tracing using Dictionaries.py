beb8a63844bda2b670e4c4c3f4d519778cedc796
markred0430 <redilamarkanthony30@gmail.com>
2022-11-13 02:21:35 +0800

    Selection

--- Contact Tracing Using Dictionaries/Contact Tracing using Dictionaries.py
+++ Contact Tracing Using Dictionaries/Contact Tracing using Dictionaries.py
@@ -4,6 +4,7 @@ print("1 -> Add an item.")
 print("2 -> Search.")
 print("3 -> Exit (y/n)")
 #Allow the user to select item in the menu.
+selection = int(input("Select what you want to do: \n"))
 #Perform the selected option
 #Option1
 #Option2
