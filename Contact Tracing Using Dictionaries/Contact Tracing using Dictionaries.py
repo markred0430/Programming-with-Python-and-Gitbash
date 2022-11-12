c3015efba5d5dae8d476bfce8e53ffed64015496
markred0430 <redilamarkanthony30@gmail.com>
2022-11-13 02:18:13 +0800

    Display

--- Contact Tracing Using Dictionaries/Contact Tracing using Dictionaries.py
+++ Contact Tracing Using Dictionaries/Contact Tracing using Dictionaries.py
@@ -1,1 +1,10 @@
-print("Test")
+#Display a menu option
+print("Menu: " )
+print("1 -> Add an item.")
+print("2 -> Search.")
+print("3 -> Exit (y/n)")
+#Allow the user to select item in the menu.
+#Perform the selected option
+#Option1
+#Option2
+#Option3
