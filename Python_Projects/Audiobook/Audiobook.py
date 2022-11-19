b1828f2f5ac2e50483aaea9f1354cba346ff48b1
markred0430 <redilamarkanthony30@gmail.com>
2022-11-19 19:00:20 +0800

    Project done.

--- Python_Projects/Audiobook/Audiobook.py
+++ Python_Projects/Audiobook/Audiobook.py
@@ -8,18 +8,18 @@ print(Pages)
 #Step1 - Install the speech library. Terminal - pip install pyttsx3
 #Step2 - Initialize. here.
 Reader = pyttsx3.init()
-Page = PdfReader.getPage(12)
-Text = Page.extractText()
-voices = Reader.getProperty('voices')
-Reader.setProperty('voice',voices[1].id)
-Reader.say(Text)
-Reader.runAndWait()
 #Step3 - Test  speak.
 #Step4 - Download the PDF file and search it here.
 #Already downloaded the pdf.
-#Step5 - Install PyPDF2
+#Step5 - Install PyPDF2.
 #Already downloaded PyPDF2.
 #Step6 - Try to read one of the page
-
-#Step7 - Change the voice
+#Step7 - Change the voice.
+voices = Reader.getProperty('voices')
+Reader.setProperty('voice',voices[0].id) #Change the index to 1 for female voice.
 #Step8 - Use for loop to read the whole book.
+for i in range(1,Pages):
+    Page = PdfReader.getPage(i)
+    Text = Page.extractText()
+    Reader.say(Text)
+    Reader.runAndWait()
