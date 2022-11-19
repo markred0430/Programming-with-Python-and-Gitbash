f003e62e71d5dc29d529a7698ac81ad82bd84fee
markred0430 <redilamarkanthony30@gmail.com>
2022-11-19 18:19:08 +0800

    Step 2 and Step 3 accomplished.

--- Python_Projects/Audiobook.py
+++
@@ -1,9 +0,0 @@
-#This program is for the audiobook for the pdf. It will read the content.
-#Step1 - Install the speech library. Terminal - pip install pyttsx3
-#Step2 - Initialize. here.
-#Step3 - Test  speak.
-#Step4 - Download the PDF file and search it here.
-#Step5 - Install PyPDF2
-#Step6 - Try to read one of the page
-#Step7 - Change the voice
-#Step8 - Use for loop to read the whole book.
