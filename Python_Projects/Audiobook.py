91973ebc48cd2f4fc9a556d3bf4d1a209d75e96a
markred0430 <redilamarkanthony30@gmail.com>
2022-11-19 18:03:29 +0800

    Clarify

--- Python_Projects/Audiobook.py
+++ Python_Projects/Audiobook.py
@@ -1,6 +1,6 @@
 #This program is for the audiobook for the pdf. It will read the content.
 #Step1 - Install the speech library. Terminal - pip install pyttsx3
-#Step2 - Initialize.
+#Step2 - Initialize. here.
 #Step3 - Test  speak.
 #Step4 - Download the PDF file and search it here.
 #Step5 - Install PyPDF2
