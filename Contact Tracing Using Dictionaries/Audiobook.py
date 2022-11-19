550507b4c3bd1599f153da65fe8fb9037ff91f98
markred0430 <redilamarkanthony30@gmail.com>
2022-11-19 17:06:15 +0800

    Benchmarks for this Project

---
+++ Contact Tracing Using Dictionaries/Audiobook.py
@@ -0,0 +1,9 @@
+#This program is for the audiobook for the pdf. It will read the content.
+#Step1 - Install the speech library.
+#Step2 - Initialize.
+#Step3 - Test  speak.
+#Step4 - Download the PDF file and search it here.
+#Step5 - Install PyPDF2
+#Step6 - Try to read one of the page
+#Step7 - Change the voice
+#Step8 - Use for loop to read the whole book.
---
+++ Contact Tracing Using Dictionaries/String Formatting.py
@@ -0,0 +1,3 @@
+Name = str(input("Name: "))
+Age = str(input("Age: "))
+print(f"Your name is {Name} and your are {Age} years old.")
